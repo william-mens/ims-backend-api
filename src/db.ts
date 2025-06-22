9595a3da16b929722134ee6914aec6caa46481f6
William Andoh-Mensah <wandoh-mensah@MBP-13-B1-034.local>
2025-06-22 09:51:00 +0000

    fixing conection timeout

--- src/db.ts
+++ src/db.ts
@@ -3,6 +3,9 @@ import mysql from 'mysql2/promise';
 import {getDbCredentials} from './helpers/ssm'
 export const getDb = async () => {
  const creds = await getDbCredentials();
+
+ console.log('logging credentials',creds);
+ 
  
   const connection = await mysql.createConnection({
     host: creds.host,
